920d81691ad4b15e86fdf8885c97e08d1fa18b15
okcoding21 <81430894+AppSync-ai@users.noreply.github.com>
2022-08-15 22:16:02 +0530

    ds

--- app/src/main/java/koro/ai/MainActivity.java
+++ app/src/main/java/koro/ai/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-import appsync.ai.koro.DatabaseHelper;
 import appsync.ai.koro.KoroToast;
 
 public class MainActivity extends AppCompatActivity {
